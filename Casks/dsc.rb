c7caf4b1a5faa7da79ff805bb1ccad424fbaca7a
bdeb1337 <bdeb1337@jng.li>
2024-06-25 18:09:47 +0200

    Need two version vars

--- Casks/dsc.rb
+++ Casks/dsc.rb
@@ -2,10 +2,11 @@ cask "dsc" do
     arch arm: "aarch64", intel: "x86_64"
   
     version "v3.0.0-preview.8"
+    version_without_v = version.delete_prefix('v')
     sha256 arm:   "02378b85bde208238acc55290ef5b1aa3b5ca884ad514d7a6bc01334c287274c",
            intel: "fc3aaa3f0a36f48d220726895bbf8a3375c43a926df3d3c57be873313846eeef"
   
-    url "https://github.com/PowerShell/DSC/releases/download/v#{version}/DSC-#{version}-#{arch}-apple-darwin.tar.gz"
+    url "https://github.com/PowerShell/DSC/releases/download/#{version}/DSC-#{version_without_v}-#{arch}-apple-darwin.tar.gz"
     name "DSC"
     desc "Microsoft Desired State Configuration v3"
     homepage "https://github.com/PowerShell/DSC"
