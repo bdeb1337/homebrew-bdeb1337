56fd0be97341e29030d8eaeabcd352736d80e197
bdeb1337 <bdeb1337@jng.li>
2024-06-25 17:51:31 +0200

    Lets see where we go

--- Casks/dsc.rb
+++ Casks/dsc.rb
@@ -5,7 +5,7 @@ cask "dsc" do
     sha256 arm:   "02378b85bde208238acc55290ef5b1aa3b5ca884ad514d7a6bc01334c287274c",
            intel: "fc3aaa3f0a36f48d220726895bbf8a3375c43a926df3d3c57be873313846eeef"
   
-    url "https://github.com/PowerShell/DSC/releases/download/v#{version}/DSC-#{version}-#{arch}-apple-darwin.tar.gz"
+    url "https://github.com/PowerShell/DSC/releases/download/#{version}/DSC-#{version}-#{arch}-apple-darwin.tar.gz"
     name "DSC"
     desc "Microsoft Desired State Configuration v3"
     homepage "https://github.com/PowerShell/DSC"
