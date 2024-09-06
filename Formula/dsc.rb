3b25eac1c2e247046289b662ec528226d04657c4
Bob Deboutte <u0113043@kuleuven.be>
2024-09-06 11:49:51 +0200

    trying alternate intel/arm defining

--- Formula/dsc.rb
+++ Formula/dsc.rb
@@ -3,10 +3,11 @@ class Dsc < Formula
   homepage "https://github.com/PowerShell/DSC"
   version "3.0.0-preview.8"
 
-  if Hardware::CPU.intel?
+  on_intel do
     sha256 "fc3aaa3f0a36f48d220726895bbf8a3375c43a926df3d3c57be873313846eeef"
     url "https://github.com/PowerShell/DSC/releases/download/v3.0.0-preview.8/DSC-3.0.0-preview.8-x86_64-apple-darwin.tar.gz"
-  elsif Hardware::CPU.arm?
+  end
+  on_arm do
     sha256 "02378b85bde208238acc55290ef5b1aa3b5ca884ad514d7a6bc01334c287274c"
     url "https://github.com/PowerShell/DSC/releases/download/v3.0.0-preview.8/DSC-3.0.0-preview.8-aarch64-apple-darwin.tar.gz"
   end
