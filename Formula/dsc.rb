4a2afe18f29f1701b7303d987b0a4afe6995ee85
Bob Deboutte <u0113043@kuleuven.be>
2024-09-06 12:18:49 +0200

    define arch as empty string and fix sha256 assignments

--- Formula/dsc.rb
+++ Formula/dsc.rb
@@ -3,13 +3,15 @@ class Dsc < Formula
   homepage "https://github.com/PowerShell/DSC"
   version "3.0.0-preview.8"
 
+  arch = ""
+
   on_macos do
     if Hardware::CPU.intel?
       arch = "x86_64-apple-darwin"
-      sha256 "fc3aaa3f0a36f48d220726895bbf8a3375c43a926df3d3c57be873313846eeef"
+      sha256 = "fc3aaa3f0a36f48d220726895bbf8a3375c43a926df3d3c57be873313846eeef"
     elsif Hardware::CPU.arm?
       arch = "aarch64-apple-darwin"
-      sha256 "02378b85bde208238acc55290ef5b1aa3b5ca884ad514d7a6bc01334c287274c"
+      sha256 = "02378b85bde208238acc55290ef5b1aa3b5ca884ad514d7a6bc01334c287274c"
     end
   end
 
@@ -19,7 +21,7 @@ class Dsc < Formula
       sha256 "677264514d58b339f75795d2627f8c288642c096db8c6c544fd673d684044e15"
     elsif Hardware::CPU.arm?
       arch = "aarch64-unknown-linux-gnu"
-      sha256 "668f15f564655145b3c9448c5c4c410521a777faa09117b36cccd94e00165f80"
+      sha256 = "668f15f564655145b3c9448c5c4c410521a777faa09117b36cccd94e00165f80"
     end
   end
 
