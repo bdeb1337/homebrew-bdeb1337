5ab494e22a7ce64aa376ce18c611fb9135ab0bf5
Bob Deboutte <u0113043@kuleuven.be>
2024-09-06 12:40:17 +0200

    i cant

--- Formula/dsc.rb
+++ Formula/dsc.rb
@@ -5,18 +5,18 @@ class Dsc < Formula
 
   if OS.mac?
     if Hardware::CPU.intel?
-      arch "x86_64-apple-darwin"
+      arch = "x86_64-apple-darwin"
       sha256 "fc3aaa3f0a36f48d220726895bbf8a3375c43a926df3d3c57be873313846eeef"
     elsif Hardware::CPU.arm?
-      arch "aarch64-apple-darwin"
+      arch = "aarch64-apple-darwin"
       sha256 "02378b85bde208238acc55290ef5b1aa3b5ca884ad514d7a6bc01334c287274c"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
-      arch "x86_64-unknown-linux-gnu"
+      arch = "x86_64-unknown-linux-gnu"
       sha256 "677264514d58b339f75795d2627f8c288642c096db8c6c544fd673d684044e15"
     elsif Hardware::CPU.arm?
-      arch "aarch64-unknown-linux-gnu"
+      arch = "aarch64-unknown-linux-gnu"
       sha256 "668f15f564655145b3c9448c5c4c410521a777faa09117b36cccd94e00165f80"
     end
   end
